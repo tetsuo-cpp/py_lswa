5c9562786d9e19f7a61279fcdd6413ce59d64a57
Alex Cameron <alex.cameron@10gen.com>
2021-02-02 02:33:31 +0000

    Print out every 1000th key for debugging purposes

--- scanner.py
+++ scanner.py
@@ -26,9 +26,13 @@ def scan():
             result = lswa.replace_one({'_id': 2}, doc2)
             assert result.matched_count == 1
 
+            counter = 0
             while True:
                 id_val = random.randint(1, 10000)
                 doc = lswa.find_one({'_id': id_val})
+                counter += 1
+                if counter % 1000 == 0:
+                    print(counter)
                 # for val in lswa.find():
                 #     # print('scan: Iterated {}'.format(val['contents'][0]))
                 #     pass
